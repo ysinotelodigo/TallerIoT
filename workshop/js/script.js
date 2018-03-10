02b5af767c8f9a0ddd0021a507324c8cad9f9f0e
Alvaro <ysinotelodigo@gmail.com>
2018-03-10 12:11:28 +0100

    No se agregaron todos las modificaciones de ficheros

--- workshop/js/script.js
+++ workshop/js/script.js
@@ -1,6 +1,7 @@
-function copyToClipboard(element) {
+function copyToClipboard(element,button) {
     var $temp = $("<textarea>");
-    $("body").append($temp);
+   // $("body").append($temp);
+    $(button).after($temp);
     $temp.val($(element).text().replace(/  /g,"")).select();
     document.execCommand("copy");
     $temp.remove();
